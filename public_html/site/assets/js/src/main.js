1c8898e9da42cf1474b41cbc0b45ffe4c7c91ae4
carlos (el-cajon) <padi_05@yahoo.com>
2016-08-01 17:39:29 -0400

    Small updates to page content

--- public_html/site/assets/js/src/main.js
+++ public_html/site/assets/js/src/main.js
@@ -1,2 +1,10 @@
-(function() {
-})();
+(function($) {
+	$(document).ready(function() {
+		$('.page-title')
+		.add('#main-content')
+		.add($('#main-content').children())
+		.each(function(i, el) {
+			setTimeout(function() { el.style.opacity = '1'; }, i * 100);
+		});
+	});
+})(jQuery);
