93a5df345fc374e1b0da596f0bf8345bf6613c7b
carlos <padi_05@yahoo.com>
2016-08-08 15:23:41 -0400

    Zenphoto image url for poster images

--- public_html/site/templates/basic-page.php
+++ public_html/site/templates/basic-page.php
@@ -2,6 +2,9 @@
 $vars = compact('page','user','config');
 $vars['menu'] = $pages->get('/')->menuHtml();
 
+$GLOBALS['twig']->addFunction(new Twig_SimpleFunction('imageUrl', function($album, $filename, $size=980) {
+	return sprintf('%szenphoto/zp-core/i.php?a=%s&i=%s&s=%d&cw=0&ch=0&q=75', $GLOBALS['wire']->config->urls->root, $album, $filename, $size);
+}));
 $GLOBALS['twig']->addFunction(new Twig_SimpleFunction('files', function($pattern) {
 	return array_map('basename', glob($pattern));
 }));
