2f19b5ba95f2afbfb95aee5161b13400839aaee9
carlos <padi_05@yahoo.com>
2016-08-12 21:38:28 -0400

    Small slideshow updates
    Smaller nav buttons
    Autoplay
    Nav fade on hover

--- public_html/site/assets/js/src/components/slideshow.js
+++ public_html/site/assets/js/src/components/slideshow.js
@@ -1,7 +1,8 @@
 (function($) {
-
 	$(document).ready(function() {
-		$('.slideshow').each(function() {
+	
+		var $slideshows = $('.slideshow');
+		$slideshows.each(function() {
 			var $s = $(this);
 			var $nav = $s.find('nav > button');
 			setTimeout(function() { $s.find('.images').slick({
@@ -9,5 +10,14 @@
 				nextArrow: $nav.last()
 			}); }, 100);
 		});
+		
+		if ($slideshows.length === 1) {
+			setTimeout(function() {
+				var slick = $slideshows.find('.images').slick('getSlick');
+				$slideshows.on('mouseenter', slick.pause.bind(slick)).on('mouseleave', slick.play.bind(slick));
+				setTimeout(slick.play.bind(slick), 3000);
+			}, 200);
+		}
+	
 	});
 })(jQuery);
