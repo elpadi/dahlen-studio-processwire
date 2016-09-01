d3950485c2f4de3bfb009cce54e2fd9e06e2a30a
Carlos Padilla <padi_05@yahoo.com>
2016-09-01 00:36:30 -0400

    Better autohide on submenus

--- public_html/site/assets/src/js/components/menu.js
+++ public_html/site/assets/src/js/components/menu.js
@@ -2,12 +2,14 @@
 	$(document).ready(function() {
 		var $menu = $('#main-menu').find('a').first().siblings('ul');
 		var $submenus = $menu.find('a + ul');
+		var autohideTimeoutId = 0;
 
 		/* Make the menu tall enough to show every submenu item */
 		$menu.css('height', ($menu.height() + Math.max.apply(window, $menu.find('ul').map(function(i, ul) { return ul.offsetHeight; })) + 20) + 'px');
 
 		$(document).on('click', function(e) {
 			if (!$menu.get(0).contains(e.target)) {
+				clearTimeout(autohideTimeoutId);
 				$submenus.removeClass('open');
 				document.body.classList.remove('submenu-open');
 			}
@@ -15,6 +17,7 @@
 
 		$menu.on('click', 'a', function(e) {
 			var $button = $(this);
+			clearTimeout(autohideTimeoutId);
 			if ($button.next().is('ul')) {
 				e.preventDefault();
 				$submenus.not($button.next()).removeClass('open');
@@ -25,17 +28,14 @@
 			}
 		});
 
-		(function() {
-			var timeoutId = 0;
-			$submenus.on('mouseenter', function() {
-				clearTimeout(timeoutId);
-			}).on('mouseleave', function() {
-				timeoutId = setTimeout(function() {
-					$submenus.removeClass('open');
-					document.body.classList.remove('submenu-open');
-				}, 1000);
-			});
-		})();
+		$submenus.on('mouseenter', function() {
+			clearTimeout(autohideTimeoutId);
+		}).on('mouseleave', function() {
+			autohideTimeoutId = setTimeout(function() {
+				$submenus.removeClass('open');
+				document.body.classList.remove('submenu-open');
+			}, 1000);
+		});
 
 	});
 })(jQuery);
