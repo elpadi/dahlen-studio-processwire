2e973a8bf69d58c186a2c92daf1868c50d2d74cd
carlos (minimal) <padi_05@yahoo.com>
2017-11-08 17:25:29 -0500

    Lightbox for still slideshows

--- public_html/site/assets/src/js/components/slideshow.js
+++ public_html/site/assets/src/js/components/slideshow.js
@@ -8,7 +8,7 @@
 			setTimeout(function() { $s.find('.images').slick({
 				pauseOnFocus: false,
 				pauseOnHover: false,
-				autoplayDuration: 1000,
+				autoplayDuration: 500,
 				lazyLoad: $slideshows.length === 1 ? 'progressive' : 'ondemand',
 				prevArrow: $nav.first(),
 				speed: 200,
@@ -19,7 +19,8 @@
 		if ($slideshows.length === 1) {
 			setTimeout(function() {
 				var slick = $slideshows.find('.images').slick('getSlick');
-				$slideshows.find('img').on('mouseenter', slick.pause.bind(slick)).on('mouseleave', slick.play.bind(slick));
+				$.featherlight.defaults.beforeOpen = slick.pause.bind(slick);
+				$.featherlight.defaults.beforeClose = slick.play.bind(slick);
 				setTimeout(slick.play.bind(slick), 2000);
 			}, 200);
 			Music.play(JSON.parse($slideshows[0].dataset.music).map(function(name) { return ASSETS_URL + 'slideshows/' + $slideshows[0].dataset.name + '/' + name; }));
